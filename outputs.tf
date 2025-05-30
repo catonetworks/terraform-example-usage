4f42b1da6bbafba95158565d579e4782dc675745
Justin Richert <justin.richert@catonetworks.com>
2025-05-30 07:51:03 -0500

    Comments and CleanUp

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "vpc3_public_ips_from_tgw_test_env" {
-  description = "Public IPs of VPC 3 servers in the test_env module"
+  description = "Public IPs of Public VPC servers in the test environment"
   value = var.build_aws_vsocket_tgw_test_env ? module.vsocket-aws-tgw[0].vpc3_public_ips_from_test_env : {}
 }
 
 output "vpc3_public_ips_from_tgw_ha_test_env" {
-  description = "Public IPs of VPC 3 servers in the test_env module"
+  description = "Public IPs of Public VPC servers in the test environment"
   value = var.build_aws_vsocket_tgw_ha_test_env ? module.build_aws_vsocket_tgw_ha_module[0].vpc3_public_ips_from_test_env : {}
 
 }
